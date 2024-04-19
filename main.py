c098114cc1ea7eabfd8bd1b51fe11e4e0a9ae125
FP-Tainan <fp.tainan@gmail.com>
2024-04-18 22:22:21 -0300

    Feat : html Index base

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
-from flask import Flask
+from flask import Flask,url_for, render_template
 
+#inicializacao
 app = Flask(__name__)
 
+#rotas
+@app.route('/')
+def ola_mundo():
+    titulo = "Gestão de usuários"
+    usuarios = [
+        {"nome": "Guilherme","Membro_ativo": True},
+        {"nome": "João","Membro_ativo": False},
+        {"nome": "Maria","Membro_ativo": True},
+    ]
+    return render_template("index.html",titulo=titulo, usuarios=usuarios)
+
+@app.route('/sobre')
+def pagina_sobre():
+    return "aaaaaa ?"
+
+#execucao
 app.run(debug=True)
