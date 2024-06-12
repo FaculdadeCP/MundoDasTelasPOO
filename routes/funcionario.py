45c164979b305030c5340f0fb99e6c09a03591a3
KinKatsu <caiofoes@gmail.com>
2024-06-12 00:26:29 -0300

    FEAT: Criado tela de Consulta funcionario

--- routes/funcionario.py
+++ routes/funcionario.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 
-funcionario_cadastro_bp = Blueprint('funcionario', __name__) # Rota para importar no main
+funcionario_bp = Blueprint('funcionario', __name__) # Rota para importar no main
 
-@funcionario_cadastro_bp.route('/funcionario/cadastro') # Rota do site
+@funcionario_bp.route('/funcionario/cadastro') # Rota do site
 def funcionario_cadastro(): 
     return render_template('funcionario_cadastro.html') # Página que vai redirecionar
+
+
+@funcionario_bp.route('/funcionario_consulta.html') # Rota do site
+def funcionario_consulta(): 
+    return render_template('funcionario_consulta.html') # Página que vai redirecionar
